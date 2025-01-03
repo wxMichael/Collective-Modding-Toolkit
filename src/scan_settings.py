5ccd44bb5a95e97afb924876a90b05dff0a9e66e
Michael <wxMichael@users.noreply.github.com>
2025-01-02 21:20:36 -0500

    Move code to new file

---
+++ src/scan_settings.py
@@ -0,0 +1,124 @@
+from enum import Enum
+from tkinter import *
+from typing import TYPE_CHECKING
+
+from globals import *
+
+if TYPE_CHECKING:
+	from pathlib import Path
+
+	from tabs._scanner import SidePane
+
+IGNORE_FOLDERS = {
+	"bodyslide",
+	"fo4edit",
+	"robco_patcher",
+	"source",
+}
+"""These are always lowercase."""
+
+DATA_WHITELIST = {
+	"complex sorter": None,
+	"f4se": None,
+	"materials": {"bgem", "bgsm", "txt"},
+	"meshes": {
+		"bto",
+		"btr",
+		"hko",
+		"hkx",
+		"hkx_back",
+		"hkx_backup",
+		"lst",
+		"max",
+		"nif",
+		"obj",
+		"sclp",
+		"ssf",
+		"tri",
+		"txt",
+		"xml",
+	},
+	"music": {"wav", "xwm"},
+	"textures": {"dds"},
+	"scripts": {"pex", "psc", "txt", "zip"},
+	"sound": {"cdf", "fuz", "lip", "wav", "xwm"},
+	"vis": {"uvd"},
+}
+"""Keys and values are lowercase with no dot."""
+
+JUNK_FILES = {
+	"thumbs.db",
+	"desktop.ini",
+	".ds_store",
+}
+
+JUNK_FILE_SUFFIXES = (
+	".tmp",
+	".bak",
+)
+
+JUNK_FOLDERS_DATA_ROOT = {
+	"fomod",
+}
+"""These are always lowercase."""
+
+PROPER_FORMATS = {
+	# Textures
+	"bmp": ["dds"],
+	"jpeg": ["dds"],
+	"jpg": ["dds"],
+	"png": ["dds"],
+	"psd": ["dds"],
+	"tga": ["dds"],
+	# Sound
+	"mp3": ["wav", "xwm"],
+}
+"Keys and values are always lowercase with no dot."
+
+RECORD_TYPES = {
+	# Sound
+	"mp3": "Sound Descriptor (SNDR) or Music Track (MUST) ",
+}
+"Keys are always lowercase with no dot."
+
+
+class ScanSetting(Enum):
+	OverviewIssues = ("Overview Issues", TOOLTIP_SCAN_OVERVIEW)
+	Errors = ("Errors", TOOLTIP_SCAN_ERRORS)
+	WrongFormat = ("Wrong File Formats", TOOLTIP_SCAN_FORMATS)
+	LoosePrevis = ("Loose Previs", TOOLTIP_SCAN_PREVIS)
+	JunkFiles = ("Junk Files", TOOLTIP_SCAN_JUNK)
+	ProblemOverrides = ("Problem Overrides", TOOLTIP_SCAN_BAD_OVERRIDES)
+	RaceSubgraphs = ("Race Subgraphs", TOOLTIP_SCAN_RACE_SUBGRAPHS)
+
+
+class ModFiles:
+	def __init__(self) -> None:
+		self.folders: dict[Path, str] = {}
+		self.files: dict[Path, str] = {}
+		self.modules: dict[str, str] = {}
+		self.archives: dict[str, str] = {}
+
+
+class ScanSettings(dict[ScanSetting, bool]):
+	def __init__(self, side_pane: "SidePane") -> None:
+		super().__init__()
+
+		self.skip_data_scan = True
+		self.mod_files: ModFiles | None = None
+
+		non_data = {ScanSetting.OverviewIssues, ScanSetting.RaceSubgraphs}
+
+		for setting in ScanSetting:
+			self[setting] = side_pane.bool_vars[setting].get()
+			if self[setting] and setting not in non_data:
+				self.skip_data_scan = False
+
+		self.manager = side_pane.scanner_tab.cmc.game.manager
+		self.using_stage = side_pane.scanner_tab.using_stage
+		if self.manager and self.manager.name == "Mod Organizer":
+			self.skip_file_suffixes = (*self.manager.skip_file_suffixes, ".vortex_backup")
+			self.skip_directories = IGNORE_FOLDERS.union(self.manager.skip_directories)
+		else:
+			self.skip_file_suffixes = (".vortex_backup",)
+			self.skip_directories = IGNORE_FOLDERS
--- src/tabs/_scanner.py
+++ src/tabs/_scanner.py
@@ -2,7 +2,6 @@ import os
 import queue
 import threading
 import webbrowser
-from enum import Enum
 from pathlib import Path
 from tkinter import *
 from tkinter import ttk
@@ -14,121 +13,16 @@ from enums import ProblemType, SolutionType, Tab, Tool
 from globals import *
 from helpers import CMCheckerInterface, CMCTabFrame, ProblemInfo, SimpleProblemInfo
 from modal_window import TreeWindow
-from utils import copy_text, exists, is_dir, is_file, read_text_encoded, rglob
-
-IGNORE_FOLDERS = {
-	"bodyslide",
-	"fo4edit",
-	"robco_patcher",
-	"source",
-}
-"""These are always lowercase."""
-
-DATA_WHITELIST = {
-	"complex sorter": None,
-	"f4se": None,
-	"materials": {"bgem", "bgsm", "txt"},
-	"meshes": {
-		"bto",
-		"btr",
-		"hko",
-		"hkx",
-		"hkx_back",
-		"hkx_backup",
-		"lst",
-		"max",
-		"nif",
-		"obj",
-		"sclp",
-		"ssf",
-		"tri",
-		"txt",
-		"xml",
-	},
-	"music": {"wav", "xwm"},
-	"textures": {"dds"},
-	"scripts": {"pex", "psc", "txt", "zip"},
-	"sound": {"cdf", "fuz", "lip", "wav", "xwm"},
-	"vis": {"uvd"},
-}
-"""Keys and values are lowercase with no dot."""
-
-JUNK_FILES = {
-	"thumbs.db",
-	"desktop.ini",
-	".ds_store",
-}
-
-JUNK_FILE_SUFFIXES = (
-	".tmp",
-	".bak",
+from scan_settings import (
+	DATA_WHITELIST,
+	JUNK_FILE_SUFFIXES,
+	JUNK_FILES,
+	PROPER_FORMATS,
+	ModFiles,
+	ScanSetting,
+	ScanSettings,
 )
-
-JUNK_FOLDERS_DATA_ROOT = {
-	"fomod",
-}
-"""These are always lowercase."""
-
-PROPER_FORMATS = {
-	# Textures
-	"bmp": ["dds"],
-	"jpeg": ["dds"],
-	"jpg": ["dds"],
-	"png": ["dds"],
-	"psd": ["dds"],
-	"tga": ["dds"],
-	# Sound
-	"mp3": ["wav", "xwm"],
-}
-"Keys and values are always lowercase with no dot."
-
-RECORD_TYPES = {
-	# Sound
-	"mp3": "Sound Descriptor (SNDR) or Music Track (MUST) ",
-}
-"Keys are always lowercase with no dot."
-
-
-class ScanSetting(Enum):
-	OverviewIssues = ("Overview Issues", TOOLTIP_SCAN_OVERVIEW)
-	Errors = ("Errors", TOOLTIP_SCAN_ERRORS)
-	WrongFormat = ("Wrong File Formats", TOOLTIP_SCAN_FORMATS)
-	LoosePrevis = ("Loose Previs", TOOLTIP_SCAN_PREVIS)
-	JunkFiles = ("Junk Files", TOOLTIP_SCAN_JUNK)
-	ProblemOverrides = ("Problem Overrides", TOOLTIP_SCAN_BAD_OVERRIDES)
-	RaceSubgraphs = ("Race Subgraphs", TOOLTIP_SCAN_RACE_SUBGRAPHS)
-
-
-class ModFiles:
-	def __init__(self) -> None:
-		self.folders: dict[Path, str] = {}
-		self.files: dict[Path, str] = {}
-		self.modules: dict[str, str] = {}
-		self.archives: dict[str, str] = {}
-
-
-class ScanSettings(dict[ScanSetting, bool]):
-	def __init__(self, side_pane: "SidePane") -> None:
-		super().__init__()
-
-		self.skip_data_scan = True
-		self.mod_files: ModFiles | None = None
-
-		non_data = {ScanSetting.OverviewIssues, ScanSetting.RaceSubgraphs}
-
-		for setting in ScanSetting:
-			self[setting] = side_pane.bool_vars[setting].get()
-			if self[setting] and setting not in non_data:
-				self.skip_data_scan = False
-
-		self.manager = side_pane.scanner_tab.cmc.game.manager
-		self.using_stage = side_pane.scanner_tab.using_stage
-		if self.manager and self.manager.name == "Mod Organizer":
-			self.skip_file_suffixes = (*self.manager.skip_file_suffixes, ".vortex_backup")
-			self.skip_directories = IGNORE_FOLDERS.union(self.manager.skip_directories)
-		else:
-			self.skip_file_suffixes = (".vortex_backup",)
-			self.skip_directories = IGNORE_FOLDERS
+from utils import copy_text, exists, is_dir, is_file, read_text_encoded, rglob
 
 
 class ScannerTab(CMCTabFrame):
